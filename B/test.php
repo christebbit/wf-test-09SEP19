61b7a3696e287972cd41226ebdaa6072e85268dc
Chris Tebbit <chris@tebb.it>
2019-09-11 16:29:41 +0100

    Test B complete

---
+++ B/test.php
@@ -0,0 +1,30 @@
+<?php
+    include('ListNode.php');
+    include('LinkedList.php');
+
+    $ll = new LinkedList();
+
+    $ll->add('Adama')
+            ->add('Roslin')
+            ->add('Baltar')
+            ->add('Valerii')
+            ->add('Thrace')
+            ->add('Tigh');
+
+    echo PHP_EOL;
+
+    echo "List Forwards:  ";
+    $ll->readList();
+
+    echo "List Backwards:  ";
+    $ll->readListBackwards();
+
+
+    $ll->deleteNode('Valerii');
+    echo "Item deleted" . PHP_EOL;
+    echo "List Forwards:  ";
+    $ll->readList();
+
+    echo "List Backwards:  ";
+    $ll->readListBackwards();
+?>
